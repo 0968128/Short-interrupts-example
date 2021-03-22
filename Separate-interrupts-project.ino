2fecc91d75df52450b0625b4537326139ef9a0d7
Lucas Hijman <0968128@hr.nl>
2021-03-23 00:34:12 +0100

    .ino file
    added main file with interrupts functionality

---
+++ Separate-interrupts-project.ino
@@ -0,0 +1,25 @@
+#include <Adafruit_CircuitPlayground.h>
+
+volatile bool mailboxFlag;
+
+void setup() {
+  // put your setup code here, to run once:
+  CircuitPlayground.begin();
+  attachInterrupt(digitalPinToInterrupt(9), ISR_button, FALLING);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if(mailboxFlag) {
+    CircuitPlayground.setPixelColor(9, 0, 255, 128);
+
+    // Set to false again, until the next change
+    mailboxFlag = false;
+  }
+}
+
+void ISR_button() {
+  Serial.println(CircuitPlayground.temperature());
+  mailboxFlag = true;
+}
